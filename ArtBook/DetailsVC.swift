aebb1d57a40264303722c3ca6a4de334d6ae237a
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-09 18:40:05 +0300

    veri aktarımı

--- ArtBook/DetailsVC.swift
+++ ArtBook/DetailsVC.swift
@@ -15,8 +15,19 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     @IBOutlet weak var artistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
     
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if chosenPainting != "" {
+            
+            //Core Data
+            
+        } else {
+            
+        }
 
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
