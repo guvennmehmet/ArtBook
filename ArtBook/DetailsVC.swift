ca4274b4342e31beeca261404f19eccf965d9ff5
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-09 18:48:06 +0300

    detayları göstermek

--- ArtBook/DetailsVC.swift
+++ ArtBook/DetailsVC.swift
@@ -23,7 +23,44 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         
         if chosenPainting != "" {
             
-            //Core Data
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+               let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                }
+
+            } catch{
+                print("error")
+            }
             
         } else {
             
